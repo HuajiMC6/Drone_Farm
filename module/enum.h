c46765704a3cba12e3963b15682cabb78a6d3c9a
PiggyHanHan <2864625260@qq.com>
2026-04-03 16:54:13 +0800

    Add files via upload

---
+++ module/enum.h
@@ -0,0 +1,36 @@
+#ifndef ENUM_H
+#define ENUM_H
+
+typedef enum{//作物种类
+    CROP_TYPE_WHEAT,
+    CROP_TYPE_RICE,
+    CROP_TYPE_CORN,
+    CROP_TYPE_NONE//没种
+}crop_type_t;
+
+typedef enum{//虫害种类
+    CROP_DAMAGE_APHID,
+    CROP_DAMAGE_MITE,
+    CROP_DAMAGE_LEAFROLLER,
+    CROP_DAMAGE_LOCUST,
+    CROP_DAMAGE_NONE//便于遍历
+}crop_damage_t;
+
+typedef enum{//农药种类
+    CROP_PESTICIDE_APHICIDE,    // 杀蚜剂（对应蚜虫）
+    CROP_PESTICIDE_ACARICIDE,   // 杀螨剂（对应螨虫）
+    CROP_PESTICIDE_LEAFROLLERICIDE, // 杀卷叶蛾剂（对应卷叶蛾）
+    CROP_PESTICIDE_LOCUSTICIDE, // 杀蝗剂（对应蝗虫）
+    CROP_PESTICIDE_NONE         // 无农药（对应无虫害）
+}crop_pesticide_t;
+
+typedef enum{//生长阶段
+    CROP_STAGE_SEED,//0-10
+    CROP_STAGE_YOUNG,//10-30
+    CROP_STAGE_GROW,//30-70
+    CROP_STAGE_BLOOM,//70-90
+    CROP_STAGE_RIPE,//90-100
+    CROP_STAGE_READY//100
+}crop_stage_t;
+
+#endif
